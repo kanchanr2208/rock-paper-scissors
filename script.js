1539276a9ac9d3db0a16ec8d36606c02287aa216
kanchanr2208 <267316562+kanchanr2208@users.noreply.github.com>
2026-06-17 14:49:10 +0530

    Final commit

--- script.js
+++ script.js
@@ -1,62 +1,66 @@
-let userSelection = "";
-let computerSelection = "";
-let roundWinnerName = document.querySelector(".round-winner")
-
-
 let userScore = 0;
 let computerScore = 0;
 let round = 1;
 
+let roundNumber = document.querySelector(".round-number");
+
 let userScoreDisplay = document.querySelector(".user-score-display");
 let computerScoreDisplay = document.querySelector(".compter-score-display");
 
-userScoreDisplay.textContent = userScore;
-computerScoreDisplay.textContent = computerScore;
-
+let userSelectionDiv = document.querySelector("#user-options-div");
 
 let userChoice = document.querySelector(".user-choice");
 let computerChoice = document.querySelector(".computer-choice");
 
-let userSelectionDiv = document.querySelector("#user-options-div");
-
-let roundResultExplanation = document.querySelector(".round-result-explanation");
-roundResultExplanation.style.display = "none";
-
 let choicesDisplay = document.querySelector(".choices-display");
-choicesDisplay.style.display = "none";
-
-
-let playNextRound = document.querySelector("#play-next-round");
-playNextRound.style.display = "none";
-
-let scoreboardMessage = document.querySelector(".scoreboard-message");
-scoreboardMessage.style.display = "none";
-
-let winner = document.querySelector(".winner");
+let roundResultExplanation = document.querySelector(".round-result-explanation");
 
 let roundResult = document.querySelector(".round-result");
-roundResult.style.display = "none";
+
+let roundWinnerName = document.querySelector(".round-winner")
+
+let playNextRound = document.querySelector("#play-next-round");
+let scoreboardMessage = document.querySelector(".scoreboard-message");
 
 let rock = document.querySelector("#rock");
 let paper = document.querySelector("#paper");
 let scissors = document.querySelector("#scissors");
 
-let roundNumber = document.querySelector(".round-number");
+let userSelection = "";
+let computerSelection = "";
 
-function usersPick() {
-    userSelectionDiv.addEventListener("click", e => {
-        e.preventDefault();
-        userSelection = e.target.value;
-        userChoice.textContent = String(userSelection);
-        rock.disabled = true;
-        paper.disabled = true;
-        scissors.disabled = true;
+userScoreDisplay.textContent = userScore;
+computerScoreDisplay.textContent = computerScore;
 
-        computersPick();
-        roundWinner(userSelection, computerSelection)
+roundResultExplanation.style.display = "none";
+choicesDisplay.style.display = "none";
+playNextRound.style.display = "none";
+scoreboardMessage.style.display = "none";
+roundResult.style.display = "none";
 
-    })
-}
+
+userSelectionDiv.addEventListener("click", e => {
+    e.preventDefault();
+    userSelection = e.target.value;
+    userChoice.textContent = String(userSelection);
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true; 
+
+    computersPick();
+    roundWinner(userSelection, computerSelection);
+
+    if (round > 5) {
+        playNextRound.disabled = true;
+        scoreboardMessage.style.display = "flex";
+        
+        if (userScore > computerScore) {
+            scoreboardMessage.textContent = "You won the game!";
+        } else if (computerScore > userScore) {
+            scoreboardMessage.textContent = "Computer won the game!";
+        }
+    }
+})
 
 function computersPick() {
     let randomNumber = Math.floor(Math.random()*3) + 1;
@@ -77,7 +81,6 @@ function computersPick() {
             computerChoice.textContent = "scissors";
             break;
     }
-    console.log(computerSelection)
     return(computerSelection);
 
 }
@@ -92,73 +95,55 @@ function roundWinner(user, computer) {
         roundResultExplanation.textContent = "You both chose " + user + ". This game is a draw. This round will be replayed." 
         roundWinnerName.textContent= "No one";
 
-    } else if (user == "rock" && computer == "paper") {
-        roundResultExplanation.textContent = computer + " covers " + user + "."
-        computerScore = computerScore + 1;
-        roundWinnerName.textContent = "Computer";
+    } else if (user != computer) {
         round = round + 1;
-
-
-    } else if (user == "rock" && computer == "scissors") {
-        roundResultExplanation.textContent = user + " breaks " + computer + "."
-        userScore = userScore + 1;
-        roundWinnerName.textContent = "You";
-        round = round + 1;
-
-    } else if (user == "paper" && computer == "rock") {
-        roundResultExplanation.textContent = user + " covers " + computer + "."
-        userScore = userScore + 1;
-        roundWinnerName.textContent = "You";
-        round = round + 1;
-
-    } else if (user == "paper" && computer == "scissors") {
-        roundResultExplanation.textContent = computer + " cut " + user + "."
-        computerScore = computerScore + 1;
-        roundWinnerName.textContent = "Computer";
-        round = round + 1;
-
-    } else if (user == "scissors" && computer == "rock") {
-        roundResultExplanation.textContent = computer + " breaks " + user + "."
-        computerScore = computerScore + 1;
-        roundWinnerName.textContent = "Computer";
-        round = round + 1;
-
-    } else if (user == "scissors" && computer == "paper") {
-        roundResultExplanation.textContent = user + " cut " + computer + "."
-        userScore = userScore + 1;
-        roundWinnerName.textContent = "You";
-        round = round + 1;
-    }
-    userScoreDisplay.textContent = userScore;
-    computerScoreDisplay.textContent = computerScore;  
-
-    if (round > 5) {
-        playNextRound.disabled = true;
-        scoreboardMessage.style.display = "flex";
         
-        if (userScore > computerScore) {
-            winner.textContent = "You";
-        } else if (computerScore > userScore) {
-            winner.textContent = "Computer";
-        }
+        if (user == "rock" && computer == "paper") {
+            roundResultExplanation.textContent = computer + " covers " + user + "."
+            computerScore = computerScore + 1;
+            roundWinnerName.textContent = "Computer";
+
+        } else if (user == "rock" && computer == "scissors") {
+            roundResultExplanation.textContent = user + " breaks " + computer + "."
+            userScore = userScore + 1;
+            roundWinnerName.textContent = "You";
+
+        } else if (user == "paper" && computer == "rock") {
+            roundResultExplanation.textContent = user + " covers " + computer + "."
+            userScore = userScore + 1;
+            roundWinnerName.textContent = "You";
+
+        } else if (user == "paper" && computer == "scissors") {
+            roundResultExplanation.textContent = computer + " cut " + user + "."
+            computerScore = computerScore + 1;
+            roundWinnerName.textContent = "Computer";
+
+        } else if (user == "scissors" && computer == "rock") {
+            roundResultExplanation.textContent = computer + " breaks " + user + "."
+            computerScore = computerScore + 1;
+            roundWinnerName.textContent = "Computer";
+
+        } else if (user == "scissors" && computer == "paper") {
+            roundResultExplanation.textContent = user + " cut " + computer + "."
+            userScore = userScore + 1;
+            roundWinnerName.textContent = "You";
+        }  
+        userScoreDisplay.textContent = userScore;
+        computerScoreDisplay.textContent = computerScore;  
     }
-    
 }
 
-usersPick();
-
-
 playNextRound.addEventListener("click", e => {
-    e.preventDefault();
-    roundNumber.textContent = round;
-    roundResult.style.display = "none";
-    scoreboardMessage.style.display = "none";
-    playNextRound.style.display = "none";
-    roundResultExplanation.style.display = "none";
-    choicesDisplay.style.display = "none";
-    rock.disabled = false;
-    paper.disabled = false;
-    scissors.disabled = false;
+        e.preventDefault();
+        roundNumber.textContent = round;
+        roundResult.style.display = "none";
+        scoreboardMessage.style.display = "none";
+        playNextRound.style.display = "none";
+        roundResultExplanation.style.display = "none";
+        choicesDisplay.style.display = "none";
+        rock.disabled = false;
+        paper.disabled = false;
+        scissors.disabled = false;
 })
 
 
